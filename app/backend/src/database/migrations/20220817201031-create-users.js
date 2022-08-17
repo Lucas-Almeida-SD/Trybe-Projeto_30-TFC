66e918a71a64e35099f1bcebb510e3534dc29cf5
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-17 18:02:32 -0300

    feat: Requisito 1 - criada migration create-users

---
+++ app/backend/src/database/migrations/20220817201031-create-users.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    })
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
