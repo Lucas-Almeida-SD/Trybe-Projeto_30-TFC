2e26997693ed800590209f709a14f98203a407df
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-18 11:56:25 -0300

    feat: Requisito 5 e 7: adicionada função de validação dos campos de login

--- app/backend/src/services/User.service.ts
+++ app/backend/src/services/User.service.ts
@@ -2,6 +2,7 @@ import generateToken from '../helpers/generateToken';
 import { ILogin } from '../interfaces/Login.interface';
 import PersistenceUserService from './PersistenceUserService';
 import PersistenceUserModel from '../database/models/PersistenceUserModel';
+import loginValidate from '../validations.ts/login.validate';
 
 export default class UserService extends PersistenceUserService {
   constructor(private model: PersistenceUserModel) {
@@ -9,6 +10,8 @@ export default class UserService extends PersistenceUserService {
   }
 
   public async login(login: ILogin): Promise<string> {
+    loginValidate.validateLoginFields(login);
+
     const { email } = login;
 
     const user = await this.model.login(email);
