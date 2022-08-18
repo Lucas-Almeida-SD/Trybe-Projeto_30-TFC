9b544a5779610646ff28fb2baed4aa369a94cb47
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-18 18:15:53 -0300

    feat: Requisito 12 - criada rota "GET /login/validate" que retorna a propriedade "role" do user

--- app/backend/src/routes/Login.route.ts
+++ app/backend/src/routes/Login.route.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import UserService from '../services/User.service';
 import UserController from '../controllers/User.controller';
 import UserModel from '../database/models/User.model';
+import authentication from '../middlewares/authentication';
 
 const loginRoute = Router();
 
 const service = new UserService(UserModel);
 const controller = new UserController(service);
 
+loginRoute.get('/validate', authentication);
 loginRoute.post('/', controller.login);
 
 export default loginRoute;
