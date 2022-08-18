839b2446aa758306418323348f6dea09629cf3bd
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-18 18:11:39 -0300

    feat: criada função que decodifica o token

---
+++ app/backend/src/helpers/decodeToken.ts
@@ -0,0 +1,17 @@
+import * as jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
+import { StatusCodes } from 'http-status-codes';
+import throwMyErrorObject from './throwMyErrorObject';
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+export default (token: string) => {
+  try {
+    const decode = jwt.verify(token, JWT_SECRET);
+    return decode;
+  } catch (err) {
+    throwMyErrorObject((err as Error).message, StatusCodes.UNAUTHORIZED);
+  }
+};
