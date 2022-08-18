bcdc82e36e82b85f798f72c4ece9e4cd13d85f11
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-18 11:57:16 -0300

    feat: criado middleware de erro

---
+++ app/backend/src/middlewares/errorMiddleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express';
+
+export default (err: Error, req: Request, res: Response, _next: NextFunction) => {
+  const { name, message } = err;
+
+  if (name === 'errorObject') {
+    const { code, message: myMessage } = JSON.parse(message);
+
+    res.status(code).json({ message: myMessage });
+  }
+  res.status(500).json({ message });
+};
