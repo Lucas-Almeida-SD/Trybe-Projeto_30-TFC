34f894d70b6a484c247d51110f6a5b2c7bca6b5b
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-18 15:36:05 -0300

    test: criado mocks loginUserRequest, invalidPasswordLoginRequest

--- app/backend/src/tests/mocks/dataMocks.ts
+++ app/backend/src/tests/mocks/dataMocks.ts
@@ -1,9 +1,14 @@
+export const loginUserRequest = {
+  email: 'admin@admin.com',
+  password: 'secret_admin',
+}
+
 export const loginUserRepositoryFindOneResponse = {
   id: 1,
-  username: 'Lucas Almeida',
-  role: 'Admin',
-  email: 'lucas@teste.com',
-  password: 'mypassword',
+  username: 'Admin',
+  role: 'admin',
+  email: 'admin@admin.com',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
 }
 
 export const nonExistingEmailLoginRequest = { password: "mypassword" }
@@ -12,5 +17,10 @@ export const nonExistingPasswordLoginRequest = { email: "teste@teste" }
 
 export const invalidEmailLoginRequest = { 
   email: "invalid@email.com",
-  password: "mypassword"
+  password: "secret_admin"
+}
+
+export const invalidPasswordLoginRequest = { 
+  email: "admin@admin.com",
+  password: "invalidpassword"
 }
