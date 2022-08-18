e1d9528a45f1a76d82776d988e4f0cc6d22ca376
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-18 14:07:34 -0300

    test: Requisito 8 - teste que contempla a validação de email(inválido) para a rota "POST /login"

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -11,7 +11,9 @@ import {
   loginUserRepositoryFindOneResponse,
   nonExistingEmailLoginRequest,
   nonExistingPasswordLoginRequest,
+  invalidEmailLoginRequest,
 } from './mocks/dataMocks';
+
 import UserRepository from '../database/models/repository/User.repository';
 
 chai.use(chaiHttp);
@@ -81,6 +83,25 @@ describe('Testes da rota "POST /login"', () => {
 
     it('Deve responder com a mensagem de erro "All fields must be filled" no corpo da resposta', () => {
       errorMessage = { message: 'All fields must be filled' };
+
+      expect(chaiHttpResponse.body).to.be.eqls(errorMessage);
+    });
+  });
+
+  describe('Será validado que não é possível realizar login com um email inválido', () => {
+    before(async () => {  
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send(invalidEmailLoginRequest);
+    });
+
+    it('Deve responder com status code "401"', () => {
+      expect(chaiHttpResponse).to.have.status(401);
+    });
+
+    it('Deve responder com a mensagem de erro "Incorrect email or password" no corpo da resposta', () => {
+      errorMessage = { message: 'Incorrect email or password' };
 
       expect(chaiHttpResponse.body).to.be.eqls(errorMessage);
     });
