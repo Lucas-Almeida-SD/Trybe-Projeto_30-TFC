c63fbb01d2306b1622b6c594554af3320c53a98f
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-18 14:26:07 -0300

    feat: Requisito 9 - adicionada validação de email inválido para a rota "POST /login"

--- app/backend/src/services/User.service.ts
+++ app/backend/src/services/User.service.ts
@@ -3,6 +3,7 @@ import { ILogin } from '../interfaces/Login.interface';
 import PersistenceUserService from './PersistenceUserService';
 import PersistenceUserModel from '../database/models/PersistenceUserModel';
 import loginValidate from '../validations.ts/login.validate';
+import { UserDTO } from '../interfaces/User.interface';
 
 export default class UserService extends PersistenceUserService {
   constructor(private model: PersistenceUserModel) {
@@ -12,11 +13,11 @@ export default class UserService extends PersistenceUserService {
   public async login(login: ILogin): Promise<string> {
     loginValidate.validateLoginFields(login);
 
-    const { email } = login;
+    const user = await this.model.login(login.email);
 
-    const user = await this.model.login(email);
+    loginValidate.validateUserExistence(user);
 
-    const token = generateToken(user);
+    const token = generateToken(user as UserDTO);
 
     return token;
   }
