de1cbf6f74aacc53f0379ded1de92c44711f1af8
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-18 10:12:50 -0300

    feat: Requisito 1 - criado repository UserRepository

---
+++ app/backend/src/database/models/repository/User.repository.ts
@@ -0,0 +1,42 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import { UserDTO } from '../../../interfaces/User.interface';
+import db from '../index';
+
+class UserRepository extends Model implements UserDTO {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+UserRepository.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  tableName: 'users',
+  timestamps: false,
+});
+
+export default UserRepository;
