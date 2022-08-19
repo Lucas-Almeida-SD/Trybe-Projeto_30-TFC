8b3e6534de2d3948254694c1533e6c3233b73a94
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-19 16:49:27 -0300

    feat: Requisito 18 - criada migration create-matches

---
+++ app/backend/src/database/migrations/20220819041552-create-matches.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('matches', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      home_team: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'teams',
+          key: 'id',
+        }
+      },
+        home_team_goals: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        away_team: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'teams',
+            key: 'id',
+          }
+        },
+        away_team_goals: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        in_progress: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+        },
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    queryInterface.dropTable('matches');
+  }
+};
