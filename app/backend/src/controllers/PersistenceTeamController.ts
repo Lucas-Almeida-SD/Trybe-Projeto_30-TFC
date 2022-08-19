1eaa6bf5ccfa30846547a4bf579ad9642614eb7f
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-19 00:11:19 -0300

    feat: adicionado método base que representa a camada controller da rota "GET /teams/:id"

--- app/backend/src/controllers/PersistenceTeamController.ts
+++ app/backend/src/controllers/PersistenceTeamController.ts
@@ -2,4 +2,5 @@ import { NextFunction, Request, Response } from 'express';
 
 export default abstract class PersistenceTeamController {
   abstract getAll(req: Request, res: Response, next: NextFunction): Promise<void>;
+  abstract getById(req: Request, res: Response, next: NextFunction): Promise<void>;
 }
