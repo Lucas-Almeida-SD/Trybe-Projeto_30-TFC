846a7e9385c53ddfbaf8d12f92665beeaeb7dddc
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-18 23:32:57 -0300

    feat: Requisito 14 - criada migration "create-teams"

---
+++ app/backend/src/database/migrations/20220819012546-create-teams.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('teams', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      team_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    })
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('teams');
+  }
+};
