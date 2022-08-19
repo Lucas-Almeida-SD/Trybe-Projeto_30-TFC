cf580568396b2a0e937d77129890a6c5be0595b3
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-19 16:54:36 -0300

    feat: criada class MatchRepository para fazer o mapeamento objeto-relacional da entidade "matches"

---
+++ app/backend/src/database/models/repository/Match.repository.ts
@@ -0,0 +1,72 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '../index';
+import TeamRepository from './Team.repository';
+
+class MatchRepository extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+
+  teamHome!: {
+    teamName: string
+  };
+
+  teamAway!: {
+    teamName: string
+  };
+}
+
+MatchRepository.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  tableName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+
+MatchRepository.belongsTo(TeamRepository, {
+  foreignKey: 'homeTeam', as: 'teamHome',
+});
+
+MatchRepository.belongsTo(TeamRepository, {
+  foreignKey: 'awayTeam', as: 'teamAway',
+});
+
+export default MatchRepository;
