6a9b98f1cda3d7eda712d98debf8463c12bb6c24
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-19 18:44:40 -0300

    feat: Requisito 18 - criada class MatchModel que contém métodos que representam as camadas model das rotas que acessam a entidade "matches"; adicionado método que representa a camada model para a rota "GET /matches"

---
+++ app/backend/src/database/models/Match.model.ts
@@ -0,0 +1,17 @@
+import MatchDTO from '../../interfaces/Match.interface';
+import PersistenceMatchModel from './PersistenceMatchModel';
+import MatchRepository from './repository/Match.repository';
+import TeamRepository from './repository/Team.repository';
+
+export default abstract class MatchModel extends PersistenceMatchModel {
+  public static async getAll(): Promise<Array<MatchDTO>> {
+    const matches = await MatchRepository.findAll({
+      include: [
+        { model: TeamRepository, as: 'teamHome', attributes: ['teamName'] },
+        { model: TeamRepository, as: 'teamAway', attributes: ['teamName'] },
+      ],
+    });
+
+    return matches;
+  }
+}
