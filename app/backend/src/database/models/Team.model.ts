a86de1b8f298082457125a851661b39c59f5a1c6
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-19 00:10:49 -0300

    feat: Requisito 16 - criado método que representa a camada model da rota "GET /teams/:id"

--- app/backend/src/database/models/Team.model.ts
+++ app/backend/src/database/models/Team.model.ts
@@ -1,11 +1,16 @@
 import { TeamDTO } from '../../interfaces/Team.interface';
-import PersistenceTeamModel from './PersistenceTeamModel';
 import TeamRepository from './repository/Team.repository';
 
-export default abstract class TeamModel extends PersistenceTeamModel {
+export default abstract class TeamModel {
   public static async getAll(): Promise<Array<TeamDTO>> {
     const teams = await TeamRepository.findAll();
 
     return teams;
+  }
+
+  public static async getById(id: number): Promise<TeamDTO> {
+    const team = await TeamRepository.findByPk(id);
+
+    return team as TeamDTO;
   }
 }
