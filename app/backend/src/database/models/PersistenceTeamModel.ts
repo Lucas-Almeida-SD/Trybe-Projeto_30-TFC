1c6ff0493455faa7d386b752d59dc3c34b61e517
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-19 00:10:28 -0300

    feat: adicionado método base que representa a camada model da rota "GET /teams/:id"

--- app/backend/src/database/models/PersistenceTeamModel.ts
+++ app/backend/src/database/models/PersistenceTeamModel.ts
@@ -2,4 +2,5 @@ import { TeamDTO } from '../../interfaces/Team.interface';
 
 export default abstract class PersistenceTeamModel {
   abstract getAll(): Promise<Array<TeamDTO>>;
+  abstract getById(id: number): Promise<TeamDTO>;
 }
