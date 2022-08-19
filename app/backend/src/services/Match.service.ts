02b6f1b7a6dfb1c5288efd3e031e798a1c8a94b0
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-19 18:47:32 -0300

    feat: Requisito 19 - criada class MatchService que contém métodos que representam as camadas service das rotas que acessam a entidade "matches"; adicionado método que representa a camada service para a rota "GET /matches"

---
+++ app/backend/src/services/Match.service.ts
@@ -0,0 +1,15 @@
+import MatchDTO from '../interfaces/Match.interface';
+import PersistenceMatchModel from '../database/models/PersistenceMatchModel';
+import PersistenceMatchService from './PersistenceMatchService';
+
+export default class MatchService extends PersistenceMatchService {
+  constructor(private model: PersistenceMatchModel) {
+    super();
+  }
+
+  public async getAll(): Promise<Array<MatchDTO>> {
+    const matches = await this.model.getAll();
+
+    return matches;
+  }
+}
