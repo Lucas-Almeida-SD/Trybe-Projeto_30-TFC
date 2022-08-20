79edf4d1578b5cc1bd6deedc46c5cae014bae738
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-20 01:15:05 -0300

    test: criado mock createMatchWithInProgressEqualTrueRequest e createMatchWithInProgressEqualTrueResponse

--- app/backend/src/tests/mocks/dataMocks.ts
+++ app/backend/src/tests/mocks/dataMocks.ts
@@ -143,3 +143,19 @@ export const matchesGetAllByProgressEqualFalseResponse = [
     }
   },
 ]
+
+export const createMatchWithInProgressEqualTrueRequest = {
+  homeTeam: 16,
+  awayTeam: 8, 
+  homeTeamGoals: 2,
+  awayTeamGoals: 2,
+}
+
+export const createMatchWithInProgressEqualTrueResponse = {
+  id: 1,
+  homeTeam: 16,
+  awayTeam: 8, 
+  homeTeamGoals: 2,
+  awayTeamGoals: 2,
+  inProgress: true,
+}
