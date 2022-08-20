064d67c523177ebc25a5d18fb03138eda3fb1d88
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-20 01:22:48 -0300

    feat: Requisito 23 - criada rota "POST /matches"

--- app/backend/src/routes/Match.route.ts
+++ app/backend/src/routes/Match.route.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import MatchModel from '../database/models/Match.model';
 import MatchService from '../services/Match.service';
 import MatchController from '../controllers/Match.controller';
+import authentication from '../middlewares/authentication';
 
 const service = new MatchService(MatchModel);
 const controller = new MatchController(service);
@@ -9,5 +10,6 @@ const controller = new MatchController(service);
 const matchRoute = Router();
 
 matchRoute.get('/', controller.getAll, controller.getAllByInProgress);
+matchRoute.post('/', authentication, controller.create);
 
 export default matchRoute;
