325510cea4bf0f54f528282d843bd9aa23c6f777
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-19 23:05:08 -0300

    feat: métdodo getAllByInProgress adicionado

--- app/backend/src/database/models/PersistenceMatchModel.ts
+++ app/backend/src/database/models/PersistenceMatchModel.ts
@@ -2,4 +2,5 @@ import MatchDTO from '../../interfaces/Match.interface';
 
 export default abstract class PersistenceMatchModel {
   abstract getAll(): Promise<Array<MatchDTO>>;
+  abstract getAllByInProgress(inProgress: boolean): Promise<Array<MatchDTO>>;
 }
