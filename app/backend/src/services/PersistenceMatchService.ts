f61ca18a56e8075a704426a407903a61c26bfc33
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-20 01:51:58 -0300

    feat: adicionado método "editInProgressToFalse"

--- app/backend/src/services/PersistenceMatchService.ts
+++ app/backend/src/services/PersistenceMatchService.ts
@@ -4,4 +4,5 @@ export default abstract class PersistenceMatchService {
   abstract getAll(): Promise<Array<MatchDTO>>;
   abstract getAllByInProgress(inProgress: boolean): Promise<Array<MatchDTO>>;
   abstract create(match: MatchCreateRequest): Promise<MatchCreateResponse>;
+  abstract editInProgressToFalse(id: number): Promise<void>;
 }
