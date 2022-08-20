4a5f26c65a949ebaae58af82756c6b03214aecd3
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-20 01:18:08 -0300

    feat: realizada segregação de interface; criada interfaces MatchCreateRequest e MatchCreateResponse

--- app/backend/src/interfaces/Match.interface.ts
+++ app/backend/src/interfaces/Match.interface.ts
@@ -1,11 +1,15 @@
-export default interface MatchDTO {
-  id: number;
+export interface MatchCreateRequest {
   homeTeam: number;
   homeTeamGoals: number;
   awayTeam: number;
   awayTeamGoals: number;
-  inProgress: boolean;
+}
 
+export interface MatchCreateResponse extends MatchCreateRequest {
+  inProgress: boolean;
+}
+
+export default interface MatchDTO extends MatchCreateResponse{
   teamHome: {
     teamName: string
   };
