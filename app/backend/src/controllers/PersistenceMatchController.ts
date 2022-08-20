f229b9186c80f6b33bfd7e588d1daef08a2c282d
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-19 23:10:01 -0300

    feat: métdodo getAllByInProgress adicionado

--- app/backend/src/controllers/PersistenceMatchController.ts
+++ app/backend/src/controllers/PersistenceMatchController.ts
@@ -1,7 +1,13 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 export default abstract class PersistenceMatchController {
   abstract getAll(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void>;
+
+  abstract getAllByInProgress(
     req: Request,
     res: Response,
   ): Promise<void>;
