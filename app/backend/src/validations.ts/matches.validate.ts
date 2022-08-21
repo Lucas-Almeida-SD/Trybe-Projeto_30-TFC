96534c4b5fde8a2f4878a367c761a12c2861e417
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-21 16:38:18 -0300

    feat: Requisito 26 - criada função que valida a existência dos times

--- app/backend/src/validations.ts/matches.validate.ts
+++ app/backend/src/validations.ts/matches.validate.ts
@@ -1,4 +1,5 @@
 import { StatusCodes } from 'http-status-codes';
+import { TeamDTO } from '../interfaces/Team.interface';
 import throwMyErrorObject from '../helpers/throwMyErrorObject';
 
 const validateTeamsEquality = (homeTeam: number, awayTeam: number) => {
@@ -10,6 +11,20 @@ const validateTeamsEquality = (homeTeam: number, awayTeam: number) => {
   }
 };
 
+const validateTeamsExistence = (
+  teams: Array<TeamDTO>,
+  homeTeam: number,
+  awayTeam: number,
+) => {
+  const findHomeTeam = teams.find((team) => team.id === homeTeam);
+  const findAwayTeam = teams.find((team) => team.id === awayTeam);
+
+  if (!findHomeTeam || !findAwayTeam) {
+    throwMyErrorObject('There is no team with such id!', StatusCodes.NOT_FOUND);
+  }
+};
+
 export default {
   validateTeamsEquality,
+  validateTeamsExistence,
 };
