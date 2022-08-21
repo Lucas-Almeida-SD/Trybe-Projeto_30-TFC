c46a3c7f7ec3058a3be241f8a9948927502f4100
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-21 17:33:05 -0300

    feat: adicionado método "editGoalsNumber"

--- app/backend/src/services/PersistenceMatchService.ts
+++ app/backend/src/services/PersistenceMatchService.ts
@@ -1,8 +1,13 @@
-import MatchDTO, { MatchCreateRequest, MatchCreateResponse } from '../interfaces/Match.interface';
+import MatchDTO, {
+  MatchCreateRequest,
+  MatchCreateResponse,
+  MatchTeamGoalsNumber,
+} from '../interfaces/Match.interface';
 
 export default abstract class PersistenceMatchService {
   abstract getAll(): Promise<Array<MatchDTO>>;
   abstract getAllByInProgress(inProgress: boolean): Promise<Array<MatchDTO>>;
   abstract create(match: MatchCreateRequest): Promise<MatchCreateResponse>;
   abstract editInProgressToFalse(id: number): Promise<void>;
+  abstract editGoalsNumber(id: number, teamGoals: MatchTeamGoalsNumber): Promise<void>;
 }
