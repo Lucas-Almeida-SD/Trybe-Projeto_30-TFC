e47bd9999136586ae77f56f42a7b23aff369792b
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-22 01:16:51 -0300

    feat: criada class PersistenceLeaderboardService que serve de base para a classe das camadas service de Leaderboard; adicionado método "getAllByHomeTeam"

---
+++ app/backend/src/services/PersistenceLeaderboardService.ts
@@ -0,0 +1,5 @@
+import { LeaderboardDTO } from '../interfaces/Leaderboard.interface';
+
+export default abstract class PersistenceLeaderboardService {
+  abstract getAllByHomeTeam(): Promise<Array<LeaderboardDTO>>;
+}
