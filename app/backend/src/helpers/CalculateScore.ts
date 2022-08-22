718033ade1286278d11431a1d68944a6a93f2222
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-22 11:09:03 -0300

    feat: criado métodos para as condições de ordenação dos leaderboards; retiradas linhas vazias emtre os elementos do escopo dos métodos

--- app/backend/src/helpers/CalculateScore.ts
+++ app/backend/src/helpers/CalculateScore.ts
@@ -5,18 +5,13 @@ type Local = 'teamHome' | 'teamAway';
 type localGoals = 'homeTeamGoals' | 'awayTeamGoals';
 
 export default abstract class CalculateLeaderboards {
-  private static teamList(
-    local: Local,
-    matches: Array<MatchDTO>,
-  ): Array<string> {
+  private static teamList(local: Local, matches: Array<MatchDTO>): Array<string> {
     const teamList: Array<string> = [];
-
     matches.forEach((match) => {
       if (!teamList.includes(match[local].teamName)) {
         teamList.push(match[local].teamName);
       }
     });
-
     return teamList;
   }
 
@@ -27,7 +22,6 @@ export default abstract class CalculateLeaderboards {
   ): Array<MatchDTO> {
     const homeTeamMatchList: Array<MatchDTO> = matches
       .filter((match) => match[local].teamName === teamName);
-
     return homeTeamMatchList;
   }
 
@@ -47,13 +41,11 @@ export default abstract class CalculateLeaderboards {
     const teamMatchList = CalculateLeaderboards.teamMatchList(teamName, local, matches);
     const localGoals = CalculateLeaderboards.localGoals(local);
     const otherLocalGoals = CalculateLeaderboards.otherLocalGoals(local);
-
     const totalPoints = teamMatchList.reduce((acc, match) => {
       if (match[localGoals] > match[otherLocalGoals]) return acc + 3;
       if (match[localGoals] < match[otherLocalGoals]) return acc;
       return acc + 1;
     }, 0);
-
     return totalPoints;
   }
 
@@ -63,7 +55,6 @@ export default abstract class CalculateLeaderboards {
     matches: Array<MatchDTO>,
   ): number {
     const teamMatchList = CalculateLeaderboards.teamMatchList(teamName, local, matches);
-
     return teamMatchList.length;
   }
 
@@ -75,11 +66,9 @@ export default abstract class CalculateLeaderboards {
     const teamMatchList = CalculateLeaderboards.teamMatchList(teamName, local, matches);
     const localGoals = CalculateLeaderboards.localGoals(local);
     const otherLocalGoals = CalculateLeaderboards.otherLocalGoals(local);
-
     const totalVictories = teamMatchList.reduce((acc, match) => (
       (match[localGoals] > match[otherLocalGoals]) ? acc + 1 : acc
     ), 0);
-
     return totalVictories;
   }
 
@@ -91,11 +80,9 @@ export default abstract class CalculateLeaderboards {
     const teamMatchList = CalculateLeaderboards.teamMatchList(teamName, local, matches);
     const localGoals = CalculateLeaderboards.localGoals(local);
     const otherLocalGoals = CalculateLeaderboards.otherLocalGoals(local);
-
     const totalDraws = teamMatchList.reduce((acc, match) => (
       (match[localGoals] === match[otherLocalGoals]) ? acc + 1 : acc
     ), 0);
-
     return totalDraws;
   }
 
@@ -107,11 +94,9 @@ export default abstract class CalculateLeaderboards {
     const teamMatchList = CalculateLeaderboards.teamMatchList(teamName, local, matches);
     const localGoals = CalculateLeaderboards.localGoals(local);
     const otherLocalGoals = CalculateLeaderboards.otherLocalGoals(local);
-
     const totalLosses = teamMatchList.reduce((acc, match) => (
       (match[localGoals] < match[otherLocalGoals]) ? acc + 1 : acc
     ), 0);
-
     return totalLosses;
   }
 
@@ -122,11 +107,9 @@ export default abstract class CalculateLeaderboards {
   ): number {
     const teamMatchList = CalculateLeaderboards.teamMatchList(teamName, local, matches);
     const localGoals = CalculateLeaderboards.localGoals(local);
-
     const totalLosses = teamMatchList.reduce((acc, match) => (
       acc + match[localGoals]
     ), 0);
-
     return totalLosses;
   }
 
@@ -137,11 +120,9 @@ export default abstract class CalculateLeaderboards {
   ): number {
     const teamMatchList = CalculateLeaderboards.teamMatchList(teamName, local, matches);
     const otherLocalGoals = CalculateLeaderboards.otherLocalGoals(local);
-
     const goalsOwn = teamMatchList.reduce((acc, match) => (
       acc + match[otherLocalGoals]
     ), 0);
-
     return goalsOwn;
   }
 
@@ -153,11 +134,9 @@ export default abstract class CalculateLeaderboards {
     const teamMatchList = CalculateLeaderboards.teamMatchList(teamName, local, matches);
     const localGoals = CalculateLeaderboards.localGoals(local);
     const otherLocalGoals = CalculateLeaderboards.otherLocalGoals(local);
-
     const goalsBalance = teamMatchList.reduce((acc, match) => (
       acc + match[localGoals] - match[otherLocalGoals]
     ), 0);
-
     return goalsBalance;
   }
 
@@ -169,26 +148,61 @@ export default abstract class CalculateLeaderboards {
     const totalPoints = CalculateLeaderboards.totalPoints(teamName, local, matches);
     const totalGames = CalculateLeaderboards.totalGames(teamName, local, matches);
     const efficiency = ((totalPoints / (totalGames * 3)) * 100).toFixed(2);
-
     return efficiency;
+  }
+
+  private static victoriesCondition(
+    nextLeaderboard: LeaderboardDTO,
+    prevLeaderboard: LeaderboardDTO,
+  ): boolean {
+    return nextLeaderboard.totalVictories > prevLeaderboard.totalVictories;
+  }
+
+  private static goalsBalanceCondition(
+    nextLeaderboard: LeaderboardDTO,
+    prevLeaderboard: LeaderboardDTO,
+  ): boolean {
+    return (nextLeaderboard.totalVictories === prevLeaderboard.totalVictories
+        && nextLeaderboard.goalsBalance > prevLeaderboard.goalsBalance
+    );
+  }
+
+  private static goalsFavorCondition(
+    nextLeaderboard: LeaderboardDTO,
+    prevLeaderboard: LeaderboardDTO,
+  ): boolean {
+    return (nextLeaderboard.totalVictories === prevLeaderboard.totalVictories
+        && nextLeaderboard.goalsBalance === prevLeaderboard.goalsBalance
+        && nextLeaderboard.goalsFavor > prevLeaderboard.goalsFavor
+    );
+  }
+
+  private static goalsOwnCondition(
+    nextLeaderboard: LeaderboardDTO,
+    prevLeaderboard: LeaderboardDTO,
+  ): boolean {
+    return (nextLeaderboard.totalVictories === prevLeaderboard.totalVictories
+        && nextLeaderboard.goalsBalance === prevLeaderboard.goalsBalance
+        && nextLeaderboard.goalsFavor === prevLeaderboard.goalsFavor
+        && nextLeaderboard.goalsOwn < prevLeaderboard.goalsOwn
+    );
   }
 
   private static sortLeaderboardList(
     leaderboardList: Array<LeaderboardDTO>,
   ): Array<LeaderboardDTO> {
     return leaderboardList.sort((next, prev) => {
-      const condition1 = next.totalVictories > prev.totalVictories;
-      const condition2 = next.totalVictories === prev.totalVictories
-        && next.goalsBalance > prev.goalsBalance;
-      const condition3 = next.goalsBalance === prev.goalsBalance
-        && next.goalsFavor > prev.goalsFavor;
-      const condition4 = next.goalsFavor > prev.goalsFavor
-        && next.goalsOwn < prev.goalsOwn;
-
-      if (condition1 || condition2 || condition3 || condition4) {
+      const victoriesCondition = CalculateLeaderboards.victoriesCondition(next, prev);
+      const goalsBalanceCondition = CalculateLeaderboards.goalsBalanceCondition(next, prev);
+      const goalsFavorCondition = CalculateLeaderboards.goalsFavorCondition(next, prev);
+      const goalsOwnCondition = CalculateLeaderboards.goalsOwnCondition(next, prev);
+      if (
+        victoriesCondition
+        || goalsBalanceCondition
+        || goalsFavorCondition
+        || goalsOwnCondition) {
         return -1;
       }
-
       return 0;
     });
   }
@@ -198,7 +212,6 @@ export default abstract class CalculateLeaderboards {
     matches: Array<MatchDTO>,
   ): Array<LeaderboardDTO> {
     const teamList = CalculateLeaderboards.teamList(local, matches);
-
     const leaderboardList = teamList.map((teamName) => ({
       name: teamName,
       totalPoints: CalculateLeaderboards.totalPoints(teamName, local, matches),
@@ -211,7 +224,6 @@ export default abstract class CalculateLeaderboards {
       goalsBalance: CalculateLeaderboards.goalsBalance(teamName, local, matches),
       efficiency: CalculateLeaderboards.efficiency(teamName, local, matches),
     }));
-
     return CalculateLeaderboards.sortLeaderboardList(leaderboardList);
   }
 }
