6f23a03496cde2ce0e9a5f1c95c8c3ac886dcf03
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-22 01:20:36 -0300

    feat: Requisito 29 - criada rota "GET /leaderboard/home"

---
+++ app/backend/src/routes/Leaderboard.route.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import LeaderboardModel from '../database/models/Leaderboard.model';
+import LeaderboardService from '../services/Leaderboard.service';
+import LeaderboardController from '../controllers/Leaderboard.controller';
+
+const leaderboardRoute = Router();
+
+const service = new LeaderboardService(LeaderboardModel);
+const controller = new LeaderboardController(service);
+
+leaderboardRoute.get('/home', controller.getAllByHomeTeam);
+
+export default leaderboardRoute;
