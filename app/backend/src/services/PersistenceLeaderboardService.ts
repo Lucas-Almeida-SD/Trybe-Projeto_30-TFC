8595b4e63f5146938b83c09416aa715e9c2fb96e
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-22 14:06:32 -0300

    feat: adicionado método base que representa a camada service da rota "GET /leaderboard/away"

--- app/backend/src/services/PersistenceLeaderboardService.ts
+++ app/backend/src/services/PersistenceLeaderboardService.ts
@@ -2,4 +2,5 @@ import { LeaderboardDTO } from '../interfaces/Leaderboard.interface';
 
 export default abstract class PersistenceLeaderboardService {
   abstract getAllByHomeTeam(): Promise<Array<LeaderboardDTO>>;
+  abstract getAllByAwayTeam(): Promise<Array<LeaderboardDTO>>;
 }
