976811d0cb23682939afd6be379a0dd68ec78078
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-22 01:19:45 -0300

    feat: Requisito 29 - criada class LeaderboardController que contém métodos que representam as camadas controller das rotas que acessam o recurso "leaderboard"; adicionado método que representa a camada controller para a rota "GET /leaderboard/home"

---
+++ app/backend/src/controllers/Leaderboard.controller.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import PersistenceLeaderboardService from '../services/PersistenceLeaderboardService';
+import PersistenceLeaderboardController from './PersistenceLeaderboardController';
+
+export default class LeaderboardController extends PersistenceLeaderboardController {
+  constructor(private service: PersistenceLeaderboardService) {
+    super();
+  }
+
+  public getAllByHomeTeam = async (
+    _req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    try {
+      const leaderboardList = await this.service.getAllByHomeTeam();
+
+      res.status(StatusCodes.OK).json(leaderboardList);
+    } catch (err) {
+      next(err);
+    }
+  };
+}
