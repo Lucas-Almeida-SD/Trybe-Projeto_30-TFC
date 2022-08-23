27f7213dd4b5ed7a5aacbced6d3158abba14aef0
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-23 02:14:47 -0300

    feat: adicionado método base que representa a camada controller da rota "GET /leaderboard/"

--- app/backend/src/controllers/PersistenceLeaderboardController.ts
+++ app/backend/src/controllers/PersistenceLeaderboardController.ts
@@ -3,4 +3,5 @@ import { NextFunction, Request, Response } from 'express';
 export default abstract class PersistenceLeaderboardController {
   abstract getAllByHomeTeam(req: Request, res: Response, next: NextFunction): Promise<void>;
   abstract getAllByAwayTeam(req: Request, res: Response, next: NextFunction): Promise<void>;
+  abstract getAllByGeneralTeam(req: Request, res: Response, next: NextFunction): Promise<void>;
 }
